456ecf1a9647142be1da7aab4cfb3a38a5489165
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2016-04-08 20:05:17 +0300

    An example of using refs to subcomponents

--- src/js/components/timer.js
+++ src/js/components/timer.js
@@ -2,14 +2,33 @@
 var Seconds = React.createClass({
   displayName: 'Timer_Seconds',
   
+  // Lifecycle
+
+  getInitialState: function ()
+  {
+    return {highlighted: false};
+  },
+
   render: function () 
   {
     return React.createElement('span', 
-      {className: 'seconds'}, 
+      {
+        className: ((!this.state.highlighted)? 'seconds' : 'seconds highlighted'),
+      }, 
       React.createElement('tt', null, this.props.seconds.toFixed(0)),
       's'
     );
   },
+
+  // Custom methods
+
+  highlight: function (flag, duration)
+  {
+    this.setState({highlighted: flag});
+    if (duration)
+        window.setTimeout(this.highlight.bind(this, !flag, 0), duration);
+  },
+    
 });
 
 var Timer = React.createClass({
@@ -25,7 +44,7 @@ var Timer = React.createClass({
   componentDidMount: function ()
   {
     console.info('A <Timer> component is mounted...');
-    this.tid = window.setInterval(this._tick.bind(this, 5.0), 5000);
+    this.tid = window.setInterval(this._tick.bind(this, 1.0), 1e+3);
     console.info('A <Timer> component has set a interval with tid=' + this.tid);
   },
 
@@ -43,10 +62,24 @@ var Timer = React.createClass({
       {className: 'timer'}, 
       // Append children (text and element nodes),
       "Elapsed (since page load): ", 
-      React.createElement(Seconds, {seconds: this.state.elapsed}),
+      React.createElement(
+        Seconds, 
+        {
+          seconds: this.state.elapsed,
+          // The <Timer> will store a reference to this child component (as
+          // soon as this is mounted to DOM).
+          ref: (c) => (this._seconds = c), // use lambda to preserve `this`
+        }),
       ' ',
-      React.createElement('button', {
-        className: 'reset-btn', onClick: this._resetTimer}, 'Reset')
+      React.createElement('button', 
+        {
+          className: 'reset-btn', 
+          onClick: this._resetTimer,
+          // The <Timer> will store a reference directly to the HTML DOM element,
+          // since this element is not represented by a React component.
+          ref: (c) => (this._button = c), // see above about lambda
+        }, 
+        'Reset')
     );
   },
 
@@ -60,8 +93,16 @@ var Timer = React.createClass({
   _resetTimer: function ()
   {
     console.info('A <Timer> is reset');
-    this.setState({elapsed: .0});
+    // Update state, highlight once the new state is re-rendered
+    this.setState({elapsed: .0}, this._highlight);
   },
+
+  _highlight: function ()
+  {
+    // We keep a reference (via `ref` special property used inside render())
+    // to our (subcomponent) seconds display.
+    this._seconds.highlight(true, 2e+3); // highlight for 2s
+  }
 });
 
 module.exports = Timer;
