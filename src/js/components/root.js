e89e56c23533e44c2ddabe3ee7040290c4b53397
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2016-04-10 23:48:42 +0300

    A routing example on the Root component

--- src/js/components/root.js
+++ src/js/components/root.js
@@ -4,27 +4,91 @@ var Greeter = require('./greeter'),
 
 var Root = React.createClass({
 
+  // Lifecycle
+
+  getInitialState: function ()
+  {
+    return {routePath: 'greet/World'};
+  },
+ 
   getDefaultProps: function ()
   {
-    return {name: 'World'};
+    return {fooPrefix: 'fooo'};
   },
-  
+
+  componentWillMount: function ()
+  {
+    var path = Root.getFragmentPath();
+    if (!path) {
+      Root.setFragmentPath(this.state.routePath);
+    } else {
+      this.setState({routePath: path});
+    }  
+  },
+
+  componentDidMount: function ()
+  {  
+    window.addEventListener("hashchange", (ev) => {
+      this.setState({routePath: Root.getFragmentPath()});
+    });
+  },
+
   render: function ()
   {
-    return (
-      <div>
-        <section id='sec-1'>
-          <h3>Section #1</h3>
-          <Greeter name={this.props.name} />
+    // A simple top-level router based on regex match
+   
+    // Note: Use arrow functions for referencing lexical `this`
+    var routes = [
+      {
+        pattern: /^foo\/(.*)$/,
+        render: (name) => (
+          <FooPortal name={name} prefix={this.props.fooPrefix} />
+        ),
+      },  
+      {
+        pattern: /^greet\/(.*)$/,
+        render: (name) => (
+          <Greeter name={name} />
+        ),
+      },
+      {
+        pattern: /^timer$/,
+        render: () => (
           <Timer />
-        </section>
-        <section id='sec-2'>
-          <h3>Section #2</h3>
-          <FooPortal name={this.props.name}/>
-        </section>
-      </div>
-    );
-  }
+        ),
+      },
+      // A catch-all route
+      {
+        pattern: /^.*$/,
+        render: () => (
+          <p>{'Cannot route to path!'}</p>
+        ),
+      }
+    ];
+    
+    // Match route and delegate to it
+    
+    var match = null;
+    for (var route of routes) {
+      match = route.pattern.exec(this.state.routePath);
+      if (match) {
+        return route.render.apply(this, match.slice(1));  
+      }
+    }
+  },
+
+  // Helpers
+
+  statics: {
+    getFragmentPath: function ()
+    {
+      return window.location.hash.substr(1);
+    },
+    setFragmentPath: function (path)
+    {
+      return (window.location.hash = path);
+    },
+  },
 });
 
 module.exports = Root
