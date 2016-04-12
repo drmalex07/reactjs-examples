a4422ef296b66bba382ffe5de6b6b5387543d0fc
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2016-04-13 01:22:46 +0300

    Include a minimal HTTP server for testing client-side application
    Conflicts:
    	src/html/example.html

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var reqparser = require('body-parser');
+var app = express();
+
+app.use(express.static('public/www'));
+app.use(reqparser.json()); // for parsing application/json
+
+app.get('/', function (req, res) {
+  res.redirect('/example.html');
+});
+
+app.post('/api/action/echo', function (req, res) {
+  res.json({message: req.body.message});
+});
+
+app.get('/api/action/get-state', function (req, res) {
+  res.json({value: 0, name: 'Supernova', color: '#dedede'});
+});
+
+app.listen(3000, function () {
+  console.info('Browse http://localhost:3000'); 
+});
+
