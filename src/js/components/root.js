3bbd1ba808abdad352d871728221c73d52a04ada
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2016-04-19 02:29:28 +0300

    An example of a Bootstrap nav together with ReactRouter

--- src/js/components/root.js
+++ src/js/components/root.js
@@ -1,12 +1,36 @@
 var React = global.React || require('react');
 var ReactRouter = global.ReactRouter || require('react-router');
+var ReactBootstrap = global.ReactBootstrap || require('react-bootstrap');
 
 var {Router, Route, IndexRoute, Link, hashHistory} = ReactRouter;
+var {Nav, NavItem} = ReactBootstrap;
 
 var Greeter = require('./greeter');
 var TodoList = require('./todo-list');
 var Timer = require('./timer');
 var Foo = require('./foo-portal');
+
+var RootMenu = React.createClass({
+  
+  render: function ()
+  {
+    return (
+      <div>
+        <Nav 
+          bsStyle='pills'
+          //activeHref={location.hash.split('?')[0]}
+          activeHref={'#' + this.props.location.pathname}
+         >
+          <NavItem href="#/greet/World">Welcome</NavItem>
+          <NavItem href="#/foo/Baz">Foo</NavItem>
+          <NavItem href="#/timer">Timer</NavItem>
+          <NavItem href="#/todos">Todos</NavItem>
+        </Nav>
+        {this.props.children}
+      </div>
+    );    
+  },
+}); 
 
 var Root = React.createClass({
  
@@ -35,26 +59,27 @@ var Root = React.createClass({
     // parameters from route!
     return (
       <Router history={hashHistory}>
-        <Route 
-          path="/"
-          component={() => (<p>About <i>something</i></p>)} 
-         />
-        <Route 
-          path="/foo/:name"
-          component={({params}) => (<Foo name={params.name} prefix={this.props.fooPrefix} />)}
-         />
-        <Route 
-          path="/greet/:name" 
-          component={({params}) => (<Greeter name={params.name} />)}
-         />
-        <Route 
-          path="/timer" 
-          component={() => (<Timer />)}
-         />
-        <Route 
-          path="/todos" 
-          component={() => (<TodoList todos={this.state.todos} />)}
-         />
+        <Route path="/" component={RootMenu}>
+          <IndexRoute
+            component={() => (<p>About <i>something</i></p>)} 
+           />
+          <Route 
+            path="foo/:name"
+            component={({params}) => (<Foo name={params.name} prefix={this.props.fooPrefix} />)}
+           />
+          <Route 
+            path="greet/:name" 
+            component={({params}) => (<Greeter name={params.name} />)}
+           />
+          <Route 
+            path="timer" 
+            component={() => (<Timer />)}
+           />
+          <Route 
+            path="todos" 
+            component={() => (<TodoList todos={this.state.todos} />)}
+           />
+        </Route>
       </Router>
     );
   },
