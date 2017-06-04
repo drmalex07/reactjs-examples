bfcfcfeea4c384e4c6ef7241b3e3a10ad334788a
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2017-06-04 15:03:21 +0300

    Minor improvements for react-router example

--- src/js/components/root.js
+++ src/js/components/root.js
@@ -1,10 +1,35 @@
 const React = require('react');
-const {HashRouter, Route, Link, NavLink, IndexRoute} = require('react-router-dom');
+const {HashRouter, Route, Switch, Link, NavLink, IndexRoute} = require('react-router-dom');
 
 var Greeter = require('./greeter');
 var TodoList = require('./todo-list');
 var Timer = require('./timer');
 var Foo = require('./foo-portal');
+
+const RootMenu = ({location}) => {
+  var links = [
+    {
+      path: "/todos", title: "Todos",
+    }, 
+    {
+      path: "/timer", title: "Timer",
+    }, 
+    {
+      path: "/greet/λαλακης", title: "Greet!",
+    },
+  ];
+  return (
+    <ul className="menu">
+      {
+        links.map(link => (
+          <li key={link.path} className={location.pathname == link.path? 'active' : ''}>
+            <NavLink to={link.path}>{link.title}</NavLink>
+          </li>
+        ))
+      } 
+    </ul>
+  );
+};
 
 class Root extends React.Component {
  
@@ -34,29 +59,27 @@ class Root extends React.Component {
       <HashRouter>
         <div className="root"> {/* a router has a single skeletal child */}
           
-          <ul className="menu">
-            <li><NavLink to="/todos">Todos</NavLink></li>
-            <li><NavLink to="/timer">Timer</NavLink></li>
-            <li><NavLink to="/greet/λαλακης">Greet!</NavLink></li>
-          </ul>
-       
+          <Route path="/" component={RootMenu} />
+
           <div className="content">
-            <Route exact={true} path="/"
-              component={() => (<p>About <i>something</i></p>)} 
-            />
-            <Route path="/foo/:name"
-              component={({match}) => (<Foo name={match.params.name} prefix={this.props.fooPrefix} />)}
-            />
-            <Route path="/greet/:name" 
-              component={({match}) => (<Greeter name={match.params.name} />)}
-            />
-            <Route path="/timer" 
-              component={() => (<Timer />)}
-            />
-            <Route path="/todos" 
-              component={() => (<TodoList todos={this.state.todos} />)}
-            />
-          </div>  
+            <Switch> {/* match only 1st of following routes */}
+              <Route exact={true} path="/"
+                component={() => (<p>About <i>something</i></p>)} 
+              />
+              <Route path="/foo/:name"
+                component={({match}) => (<Foo name={match.params.name} prefix={this.props.fooPrefix} />)}
+              />
+              <Route path="/greet/:name" 
+                component={({match}) => (<Greeter name={match.params.name} />)}
+              />
+              <Route path="/timer" 
+                component={Timer}
+              />
+              <Route path="/todos" 
+                component={() => (<TodoList todos={this.state.todos} />)}
+              />
+            </Switch>
+          </div>
         </div>
       </HashRouter>
     );
