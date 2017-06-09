c13bfe4c5d049ee70542cdf56bdb2f9317528937
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2017-06-09 15:52:04 +0300

    Fix coreui starter templates to be React-friendly

--- src/js/components/layout/header.js
+++ src/js/components/layout/header.js
@@ -8,44 +8,25 @@ class Header extends React.Component
   {
     super(props);
 
-    this.toggle = this.toggle.bind(this);
+    this._toggleDropdown = this._toggleDropdown.bind(this);
+    
     this.state = {
       dropdownOpen: false,
     };
   }
    
-  toggle() 
+  _toggleDropdown() 
   {
     this.setState({dropdownOpen: !this.state.dropdownOpen});
   }
 
-  // Fixme: do not directly modify DOM elements! 
-  
-  sidebarToggle(e) {
-    e.preventDefault();
-    document.body.classList.toggle('sidebar-hidden');
-  }
-
-  sidebarMinimize(e) {
-    e.preventDefault();
-    document.body.classList.toggle('sidebar-minimized');
-  }
-
-  mobileSidebarToggle(e) {
-    e.preventDefault();
-    document.body.classList.toggle('sidebar-mobile-show');
-  }
-
-  asideToggle(e) {
-    e.preventDefault();
-    document.body.classList.toggle('aside-menu-hidden');
-  }
-
-  render() {
+  render() 
+  {
     return (
       <header className="app-header navbar">
         
-        <button className="navbar-toggler mobile-sidebar-toggler d-lg-none" onClick={this.mobileSidebarToggle} type="button">
+        <button className="navbar-toggler mobile-sidebar-toggler d-lg-none" type="button" 
+            onClick={() => this.props.styleSidebar('mobile-show')}>
           <i className="fa fa-navicon"></i>
         </button>
         <a className="navbar-brand" href="#"></a>
@@ -54,7 +35,8 @@ class Header extends React.Component
         <ul className="nav navbar-nav d-md-down-none mr-auto">
           {/* toggle sidebar */}
           <li className="nav-item">
-            <button className="nav-link navbar-toggler sidebar-toggler" type="button" onClick={this.sidebarToggle}>
+            <button className="nav-link navbar-toggler sidebar-toggler" type="button" 
+                onClick={this.props.toggleSidebar}>
               <i className="fa fa-navicon"></i>
             </button>
           </li>       
@@ -82,32 +64,56 @@ class Header extends React.Component
             <a className="nav-link" href="#"><i className="icon-location-pin"></i></a>
           </li>
           <li className="nav-item">
-            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-              <button onClick={this.toggle} className="nav-link dropdown-toggle" data-toggle="dropdown" type="button" 
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this._toggleDropdown}>
+              <button onClick={this._toggleDropdown} className="nav-link dropdown-toggle" data-toggle="dropdown" type="button" 
                   aria-haspopup="true" aria-expanded={this.state.dropdownOpen}>
                 <img src={'img/avatar.jpg'} className="img-avatar" alt="admin@example.com"/>
                 <span className="d-md-down-none">admin</span>
               </button>
               <DropdownMenu className="dropdown-menu-right">
-                <DropdownItem header className="text-center"><strong>Account</strong></DropdownItem>
-                <DropdownItem><i className="fa fa-bell-o"></i> Updates<span className="badge badge-info">42</span></DropdownItem>
-                <DropdownItem><i className="fa fa-envelope-o"></i> Messages<span className="badge badge-success">42</span></DropdownItem>
-                <DropdownItem><i className="fa fa-tasks"></i> Tasks<span className="badge badge-danger">42</span></DropdownItem>
-                <DropdownItem><i className="fa fa-comments"></i> Comments<span className="badge badge-warning">42</span></DropdownItem>
-                <DropdownItem header className="text-center"><strong>Settings</strong></DropdownItem>
-                <DropdownItem><i className="fa fa-user"></i> Profile</DropdownItem>
-                <DropdownItem><i className="fa fa-wrench"></i> Settings</DropdownItem>
-                <DropdownItem><i className="fa fa-usd"></i> Payments<span className="badge badge-default">42</span></DropdownItem>
-                <DropdownItem><i className="fa fa-file"></i> Projects<span className="badge badge-primary">42</span></DropdownItem>
+                <DropdownItem header className="text-center">
+                  <strong>Account</strong>
+                </DropdownItem>
+                <DropdownItem>
+                  <i className="fa fa-bell-o"></i> Updates<span className="badge badge-info">42</span>
+                </DropdownItem>
+                <DropdownItem>
+                  <i className="fa fa-envelope-o"></i> Messages<span className="badge badge-success">42</span>
+                </DropdownItem>
+                <DropdownItem>
+                  <i className="fa fa-tasks"></i> Tasks<span className="badge badge-danger">42</span>
+                </DropdownItem>
+                <DropdownItem>
+                  <i className="fa fa-comments"></i> Comments<span className="badge badge-warning">42</span>
+                </DropdownItem>
+                <DropdownItem header className="text-center">
+                  <strong>Settings</strong></DropdownItem>
+                <DropdownItem>
+                  <i className="fa fa-user"></i> Profile
+                </DropdownItem>
+                <DropdownItem>
+                  <i className="fa fa-wrench"></i> Settings
+                </DropdownItem>
+                <DropdownItem>
+                  <i className="fa fa-usd"></i> Payments<span className="badge badge-default">42</span>
+                </DropdownItem>
+                <DropdownItem>
+                  <i className="fa fa-file"></i> Projects<span className="badge badge-primary">42</span>
+                </DropdownItem>
                 <DropdownItem divider />
-                <DropdownItem><i className="fa fa-shield"></i> Lock Account</DropdownItem>
-                <DropdownItem><i className="fa fa-lock"></i> Logout</DropdownItem>
+                <DropdownItem>
+                  <i className="fa fa-shield"></i> Lock Account
+                </DropdownItem>
+                <DropdownItem>
+                  <i className="fa fa-lock"></i> Logout
+                </DropdownItem>
               </DropdownMenu>
             </Dropdown>
           </li>
-          {/* toggle aside */}
+          {/* toggle aside menu */}
           <li className="nav-item d-md-down-none">
-            <button className="nav-link navbar-toggler aside-menu-toggler" type="button" onClick={this.asideToggle}>
+            <button className="nav-link navbar-toggler aside-menu-toggler" type="button" 
+                onClick={this.props.toggleAsideMenu}>
               <i className="fa fa-navicon"></i>
             </button>
           </li>
@@ -117,5 +123,12 @@ class Header extends React.Component
     );
   }
 }
+
+Header.propTypes = {
+  toggleSidebar: PropTypes.func.isRequired,
+  styleSidebar: PropTypes.func.isRequired,
+  toggleAsideMenu: PropTypes.func.isRequired,
+  styleAsideMenu: PropTypes.func.isRequired,
+};
 
 module.exports = Header;
--- src/js/components/root.js
+++ src/js/components/root.js
@@ -15,13 +15,101 @@ const routeInfo = require('../route-info');
 const history = createBrowserHistory();
 
 
+/////////////////////////////////////////////////////////////////
+//
+//    // Header options
+//    1. '.header-fixed'					- Fixed Header
+//
+//    // Sidebar options
+//    1. '.sidebar-fixed'					- Fixed Sidebar
+//    2. '.sidebar-hidden'				- Hidden Sidebar
+//    3. '.sidebar-off-canvas'		- Off Canvas Sidebar
+//    4. '.sidebar-minimized'			- Minimized Sidebar (Only icons)
+//    5. '.sidebar-compact'			  - Compact Sidebar
+//    6. '.sidebar-mobile-show'   - ??
+//
+//    // Aside options
+//    1. '.aside-menu-fixed'			- Fixed Aside Menu
+//    2. '.aside-menu-hidden'			- Hidden Aside Menu
+//    3. '.aside-menu-off-canvas'	- Off Canvas Aside Menu
+//
+//    // Footer options
+//    1. 'footer-fixed'						- Fixed footer
+//
+/////////////////////////////////////////////////////////////////
+
+
 class Root extends React.Component 
 {
-  render() {
+  constructor(props)
+  {
+    super(props);
+    
+    this._toggleSidebar = this._toggleSidebar.bind(this);
+    this._styleSidebar = this._styleSidebar.bind(this);
+    this._toggleAsideMenu = this._toggleAsideMenu.bind(this);
+    this._styleAsideMenu = this._styleAsideMenu.bind(this);
+    
+    this.state = {
+      sidebarOpen: true,
+      sidebarStyle: 'fixed', // fixed, compact, minimized, off-canvas
+      asideOpen: false,
+      asideStyle: 'fixed', // fixed, off-canvas
+    };
+  }
+
+  _toggleSidebar() 
+  {
+    this.setState({sidebarOpen: !this.state.sidebarOpen});
+  }
+
+  _styleSidebar(style)
+  {
+    if (['fixed', 'compact', 'minimized', 'off-canvas', 'mobile-show'].indexOf(style) < 0) {
+      console.warn('Ignoring unknown sidebar style: ' + style);
+      return;
+    }
+
+    this.setState({sidebarStyle: style});
+  }
+  
+  _toggleAsideMenu() 
+  {
+    this.setState({asideOpen: !this.state.asideOpen});
+  }
+
+  _styleAsideMenu(style) 
+  {
+    if (['fixed', 'off-canvas'].indexOf(style) < 0) {
+      console.warn('Ignoring unknown aside-menu style: ' + style);
+      return;
+    }
+    
+    this.setState({asideStyle: style});
+  }
+
+  render() 
+  {
+    var cssClasses = [
+      /* header-* */
+      'header-fixed',
+      /* sidebar-* */
+      this.state.sidebarOpen? null : 'sidebar-hidden',
+      'sidebar-' + (this.state.sidebarStyle || 'fixed'),
+      /* aside-menu-* */
+      this.state.asideOpen? null : 'aside-menu-hidden',
+      'aside-menu-' + (this.state.asideStyle || 'fixed'),
+    ];
+   
     return (
       <HashRouter history={history}>
-        <div className="app">
-          <Header />
+        <div className={'app ' + cssClasses.join(' ')}>
+          <Header
+            toggleSidebar={this._toggleSidebar}
+            styleSidebar={this._styleSidebar}
+            toggleAsideMenu={this._toggleAsideMenu}
+            styleAsideMenu={this._styleAsideMenu}
+           />
           <div className="app-body">
             <Route path="/" component={Sidebar} />
             <main className="main">
@@ -41,5 +129,6 @@ class Root extends React.Component
     );
   }
 }
+
 
 module.exports = Root;
